366b7b0ef16e3d5a2c1d084e402c7d6310792c7a
tye101 <tye101@uottawa.ca>
2024-09-25 23:02:21 -0400

    Add icons and reformat services

--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { hairServices, nailServices, hairRemovalServices } from '../servicesData';
+import './ServicesPage.css';
+import { NavLink } from 'react-router-dom';  // Import NavLink
 
 const ServicesPage = () => {
   return (
     <div className="container">
-      <h1>Our Services</h1>
-
       <h2>Hair Services</h2>
+      <NavLink exact to="/">
+        <img src="/hairdresser.png" alt="Hairdresser logo" className="logo" />
+      </NavLink>
       <ul>
         {hairServices.map((service, index) => (
           <li key={index}>{service.service}: {service.price}</li>
@@ -14,6 +17,9 @@ const ServicesPage = () => {
       </ul>
 
       <h2>Nail Services</h2>
+      <NavLink exact to="/">
+        <img src="/manicure.png" alt="Manicure logo" className="logo" />
+      </NavLink>
       <ul>
         {nailServices.map((service, index) => (
           <li key={index}>{service.service}: {service.price}</li>
@@ -21,6 +27,9 @@ const ServicesPage = () => {
       </ul>
 
       <h2>Hair Removal Services</h2>
+      <NavLink exact to="/">
+        <img src="/wax.png" alt="Wax logo" className="logo" />
+      </NavLink>
       <ul>
         {hairRemovalServices.map((service, index) => (
           <li key={index}>{service.service}: {service.price}</li>
