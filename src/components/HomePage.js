36dd9685a59c0e2e12d2178586f31fe1819f1fa6
tye101 <tye101@uottawa.ca>
2024-10-09 21:20:22 -0400

    Add image to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,7 @@ const HomePage = () => {
     <div className="container">
       <h2>Welcome to Wendy's Studio</h2>
       <p>Your cozy, modern hair, nail, and beauty care destination. We provide personalized services to make you feel pampered and refreshed.</p>
+      <img src="/front-store.JPG" alt="Front Store View" className='homepageImage'/>
       <button onClick={() => window.location.href = '/services'}>View Our Services</button>
     </div>
   );
